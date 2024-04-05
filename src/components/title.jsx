2a4f9b6128f65929f67d1f310413b62d07526973
Iana Khasanshina <ianakhasanshina@wifi-64-187-245-246.nyit.edu>
2024-04-05 19:13:50 -0400

    my first commit

---
+++ src/components/title.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react';
+const Title = (props) => {
+    return (
+<h1> Name: {props.name} Last Name: {props.last}</h1>
+    )
+}
+
+export default Title;
+
+    
+
